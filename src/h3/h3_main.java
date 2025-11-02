8700b851bbd57e5d5b5c94dded43310b7208b008
dkoelngit <dariushplays@yahoo.com>
2025-11-02 15:44:32 +0100

    Soweit funktioniert funktioniert alles aber es gibt fehler in der Logik wenn fix>max

--- src/h3/h3_main.java
+++ src/h3/h3_main.java
@@ -4,12 +4,18 @@ public class h3_main {
     public static void main(String[] args) {
 
         int max = 610; //   Sitzplätze von Hörsaal B 
-        int fix = 500;
+        int fix = 700;
         int wartend = 200;
 
         boolean istVoll = false; 
+        
+        //  als erstes nennen wir dem Nutzer die Kennzahlen
+        
+        System.out.println("Es gibt " + max + " Plätze.");
+        System.out.println("Davon sind " + fix + " als Fixplatz vergeben");
+        System.out.println("Es stehen " + wartend + " Leute auf der Warteliste");
 
-        //  zuerst ermitteln wird ob es überhaupt noch frei Plätze gibt 
+        //  dann ermitteln wird ob es überhaupt noch frei Plätze gibt 
 
         if (max <= fix) { 
             istVoll = true;
@@ -23,5 +29,20 @@ public class h3_main {
             System.out.println("Es gibt " + freieplätze + " freie Plätze");
         }
 
+        // nun wissen wir wie viele Plätze noch frei sind und weisen die Leute von der Warteliste diesen freien Plätzen zu
+        // bis entweder max erreicht ist oder die Warteliste leer ist
+
+        if (freieplätze > 0 && wartend > 0) {
+            System.out.println("Da es noch frei Plätze gibt werden die Leute von der Warteliste nachgerückt");
+            int zuweisen = Math.min(freieplätze, wartend);  //  hiermit ermitteln wir wie viele Leute noch von der Warteliste in den Saal passen
+            fix += zuweisen;
+            wartend -= zuweisen;
+        }
+        
+        istVoll = (fix == max); {
+            System.out.println("Der Saal ist nun voll");
+        }
+
+        System.out.println("Es stehen " + wartend + " Leute auf der Warteliste");
     }
 }
